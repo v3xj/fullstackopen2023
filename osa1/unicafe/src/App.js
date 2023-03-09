c2788d938632df2543e38fe33efc050702219f28
v3xj <vjbrah@gmail.com>
2023-03-10 00:46:54 +0200

    unicafe step2

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -1,10 +1,54 @@
 import { useState } from 'react'
 
 const App = () => {
-  // tallenna napit omaan tilaansa
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [feedback, setFeedback] = useState({
+    good: 0, neutral: 0, bad: 0
+  })
+  const [average, setAverage] = useState({
+    average: 0
+  })
+
+  const handleGoodClick = () => {
+    const newClicks = {
+      good: feedback.good + 1,
+      neutral: feedback.neutral,
+      bad: feedback.bad,
+    }
+    const newAverage = {
+      average: average.average + 1
+    }
+    setFeedback(newClicks)
+    setAverage(newAverage)
+  }
+
+  const handleNeutralClick = () => {
+    const newClicks = {
+      good: feedback.good,
+      neutral: feedback.neutral + 1,
+      bad: feedback.bad,
+      average: average.average
+    }
+    const newAverage = {
+      average: average.average
+    }
+    setFeedback(newClicks)
+    setAverage(newAverage)
+  }
+
+  const handleBadClick = () => {
+    const newClicks = {
+      good: feedback.good,
+      neutral: feedback.neutral,
+      bad: feedback.bad + 1,
+      average: average.average - 1
+    }
+    const newAverage = {
+      average: average.average - 1
+    }
+    setFeedback(newClicks)
+    setAverage(newAverage)
+  }
+
 
   return (
     <div>
@@ -12,27 +56,30 @@ const App = () => {
         <h1>give feedback</h1>
       </div>
       <div>
-        <button onClick={() => setGood(good + 1)}>
-          good
-        </button>
-        <button onClick={() => setNeutral(neutral + 1)}>
-          neutral
-        </button>
-        <button onClick={() => setBad(bad + 1)}>
-          bad
-        </button>
+        <button onClick={handleGoodClick}>good</button>
+        <button onClick={handleNeutralClick}>neutral</button>
+        <button onClick={handleBadClick}>bad</button>
       </div>
       <div>
         <h1>statistics</h1>
       </div>
       <div>
-      good {good}
+      good {feedback.good}
       </div>
       <div>
-      neutral {neutral}
+      neutral {feedback.neutral}
       </div>
       <div>
-      bad {bad}
+      bad {feedback.bad}
+      </div>
+      <div>
+        all {feedback.good + feedback.neutral + feedback.bad}
+      </div>
+      <div>
+        average {average.average / (feedback.good + feedback.neutral + feedback.bad)}
+      </div>
+      <div>
+        positive {feedback.good / (feedback.good + feedback.neutral + feedback.bad) * 100} %
       </div>
     </div>
   )
