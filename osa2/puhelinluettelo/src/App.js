1962bf8d8ad2474ab2f1fa407f46b5e0a989369f
v3xj <vjbrah@gmail.com>
2023-03-19 18:53:12 +0200

    2.9 puhelinluettelo step4

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,13 +1,31 @@
 import { useState } from 'react'
 
 const App = () => {
-  const [persons, setPersons] = useState([]) 
+  const [persons, setPersons] = useState([
+    /* TESTIDATAA */
+    /*
+    { content: 'Arto Hellas', number: '040-123456', id: 1 },
+    { content: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { content: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { content: 'Mary Poppendieck', number: '39-23-6423122', id: 4}
+    */
+    
+  ]) 
   const [newName, setNewName] = useState(
     ''
   )
   const [newNumber, setNewNumber] = useState(
     ''
   )
+  const [newFilter, setFilter] = useState(
+    ''
+  )
+  const [showAll, setShowAll] = useState(true)
+
+  const personsToShow = showAll 
+    ? persons 
+    : persons.filter(person => 
+      person.content.toLowerCase().includes(newFilter)) 
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -18,15 +36,13 @@ const App = () => {
     }
 
     const names = persons.map(person => person.content)
-
-    names.includes(newName) ? 
-    alert(`${newName} is already added to phonebook`) :
-    setPersons(persons.concat(nameObject))
+    names.includes(newName) 
+    ? alert(`${newName} is already added to phonebook`) 
+    : setPersons(persons.concat(nameObject))
 
     setNewName('')
     setNewNumber('')
   }
-
 
   const handleNameChange = (event) => {
     console.log(event.target.value)
@@ -38,9 +54,24 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleFilterChange = (event) => {
+    console.log(event.target.value)
+    setFilter(event.target.value)
+    setShowAll(false)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with: <input 
+        value={newFilter} 
+        onChange={handleFilterChange} />
+      </div>
+      <div>
+        filter debug: {newFilter}
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input 
@@ -57,10 +88,11 @@ const App = () => {
           <div>debug: {newName} {newNumber}</div>
         </div>
       </form>
-      <h2>Numbers</h2>
-        {persons.map(person =>
-          <div key={person.id}>{person.content} {person.number} </div>
-          )}
+      <div>
+        <h2>Numbers</h2>
+        {personsToShow.map(person => 
+          <div key={person.id}>{person.content} {person.number}</div>)}
+      </div>
     </div>
   )
 
