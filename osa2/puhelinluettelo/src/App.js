815a7a7a436f9bbcd5286e6b2a14fb0cb3ec10f6
v3xj <vjbrah@gmail.com>
2023-03-19 18:07:26 +0200

    2.8 puhelinluettelo step3

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -5,11 +5,15 @@ const App = () => {
   const [newName, setNewName] = useState(
     ''
   )
+  const [newNumber, setNewNumber] = useState(
+    ''
+  )
 
-  const addName = (event) => {
+  const addPerson = (event) => {
     event.preventDefault()
     const nameObject = {
       content: newName,
+      number: newNumber,
       id: persons.length + 1,
     }
 
@@ -20,31 +24,42 @@ const App = () => {
     setPersons(persons.concat(nameObject))
 
     setNewName('')
+    setNewNumber('')
   }
 
 
-  const handleInputChange = (event) => {
+  const handleNameChange = (event) => {
     console.log(event.target.value)
     setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addName}>
+      <form onSubmit={addPerson}>
         <div>
           name: <input 
           value={newName}
-          onChange={handleInputChange} />
+          onChange={handleNameChange} />
+        </div>
+        <div>
+          number: <input 
+          value={newNumber}
+          onChange={handleNumberChange} />
         </div>
         <div>
           <button type="submit">add</button>
-          <div>debug: {newName} </div>
+          <div>debug: {newName} {newNumber}</div>
         </div>
       </form>
       <h2>Numbers</h2>
         {persons.map(person =>
-          <div key={person.id}>{person.content}</div>
+          <div key={person.id}>{person.content} {person.number} </div>
           )}
     </div>
   )
