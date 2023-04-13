276dac145d57c084b97ab236b9717be6c4bff732
v3xj <vjbrah@gmail.com>
2023-04-13 17:46:16 +0300

    3.20 custom validation for number

--- osa3/puhelinluettelo_backend/models/person.js
+++ osa3/puhelinluettelo_backend/models/person.js
@@ -21,9 +21,15 @@ const personSchema = new mongoose.Schema({
   },
   number: {
     type: String,
-    required: true
+    validate: {
+      validator: function(v) {
+        return /\d{2,3}-\d{6,}/.test(v);
+      },
+      message: props => `${props.value} is not a valid phone number!`
+    },
+    required: [true, 'Phone number required']
   },
-})
+});
 
 personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
