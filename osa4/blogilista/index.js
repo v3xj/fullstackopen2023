2cbd261d6f00acf5bc33829cab6f0a9786f534b1
v3xj <vjbrah@gmail.com>
2023-04-16 22:46:59 +0300

    4.2 blog.js added

--- osa4/blogilista/index.js
+++ osa4/blogilista/index.js
@@ -1,32 +1,11 @@
 require('dotenv').config()
-const http = require('http')
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const mongoose = require('mongoose')
-
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-const mongoUrl = process.env.MONGODB_URI
-mongoose.connect(mongoUrl)
+const Blog = require('./models/blog')
 
 app.use(cors())
 app.use(express.json())
-
-blogSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
-})
 
 app.get('/api/blogs', (request, response) => {
   Blog
