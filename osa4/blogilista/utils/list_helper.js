251ca8bdd6a3f25c93371ce1f0cb7f6bb48b7623
v3xj <vjbrah@gmail.com>
2023-04-23 17:27:30 +0300

    4.6 favoriteBlog and mostBlogs

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -1,3 +1,5 @@
+const _ = require('lodash')
+
 const dummy = (blogs) => {
   return 1;
 }
@@ -10,6 +12,28 @@ const totalLikes = (blogs) => {
   return total;
 }
 
+const favoriteBlog = (blogs) => {
+  let highest = 0;
+  let mostLikes = 0;
+  blogs.map(blog => {
+    if (blog.likes > highest) {
+      console.log(blog.title + 'Likes: ' + blog.likes + ' NEW HIGHEST FOUND')
+      highest = blog.likes
+      mostLikes = blog
+    }
+  })
+  console.log(mostLikes)
+  return mostLikes
+}
+
+const mostBlogs = (blogs) => {
+  var blogsPerWriter = _.countBy(blogs, 'author')
+  var writerWithMostBlogs = _.findKey(blogsPerWriter, function(o) {
+    return o === _.max(_.map(blogsPerWriter))
+  })
+  return writerWithMostBlogs
+}
+
 module.exports = {
-  dummy, totalLikes
+  dummy, totalLikes, favoriteBlog, mostBlogs
 }
