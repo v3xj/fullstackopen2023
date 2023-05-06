36ecc444eb3f6a1a475184e1159b2a224b57c30b
v3xj <vjbrah@gmail.com>
2023-05-07 00:16:11 +0300

    4.19 POST works only with valid token

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -1,6 +1,7 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({})
@@ -19,8 +20,21 @@ blogsRouter.get('/:id', (request, response, next) => {
     .catch(error => next(error))
 })
 
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
+
 blogsRouter.post('/', async (request, response, next) => {
   const body = request.body
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
 
   if (body.likes === "") {
     body.likes = 0;
@@ -38,8 +52,6 @@ blogsRouter.post('/', async (request, response, next) => {
     })
   }
 
-  const userId = await User.findById(body.user, 'id')
-  const user = await User.findById(body.user)
   console.log(' POSTAUS USER VALUE:', body)
 
   const blog = new Blog({
@@ -47,7 +59,7 @@ blogsRouter.post('/', async (request, response, next) => {
     author: body.author,
     url: body.url,
     likes: body.likes,
-    user: userId
+    user: user.id
   })
 
   const savedBlog = await blog.save()
