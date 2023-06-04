4bf0d7e7292579d301de04800c714ae6014adbf8
v3xj <vjbrah@gmail.com>
2023-06-04 20:50:10 +0300

    5.2 local storage and log out

--- osa5/bloglist-frontend/src/App.js
+++ osa5/bloglist-frontend/src/App.js
@@ -16,6 +16,16 @@ const App = () => {
     )  
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      setUsername(user.username)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
   const handleLogin = async (event) => {
     event.preventDefault()
 
@@ -23,6 +33,10 @@ const App = () => {
       const user = await loginService.login({
         username, password,
       })
+
+      window.localStorage.setItem(
+        'loggedBlogappUser', JSON.stringify(user)
+      )
       setUser(user)
     } catch (exception) {
       setErrorMessage('wrong credentials')
@@ -33,11 +47,29 @@ const App = () => {
     console.log('logging in with', username, password)
   }
 
+  const handleLogout = async (event) => {
+    event.preventDefault()
+
+    try {
+      window.localStorage.clear()
+      setUser(null)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      setErrorMessage('logout failed')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+    console.log('logging out')
+  }
+
   if (user !== null) {
     return (
       <div>
       <h2>blogs</h2>
-      <h3>logged in as {username}</h3>
+      <h3>logged in as {username}
+      <button onClick={handleLogout}>log out</button></h3>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
