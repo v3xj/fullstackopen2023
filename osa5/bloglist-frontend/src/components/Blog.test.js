ffd6f7032196e4a464963fcc4b229307665c5787
v3xj <vjbrah@gmail.com>
2023-10-30 20:03:39 +0200

    5.14 test for url, likes and author

--- osa5/bloglist-frontend/src/components/Blog.test.js
+++ osa5/bloglist-frontend/src/components/Blog.test.js
@@ -1,6 +1,7 @@
 import React from "react"
 import "@testing-library/jest-dom"
 import { render, screen } from "@testing-library/react"
+import userEvent from "@testing-library/user-event"
 import Blog from "./Blog"
 
 test("renders content", () => {
@@ -10,11 +11,38 @@ test("renders content", () => {
     author: "testuser"
   }
 
-  render(<Blog blog={blog} />)
+  const mockHandler = jest.fn()
+
+  render(<Blog blog={blog} toggleVisibility={mockHandler}/>)
 
   screen.debug()
 
   const element = screen.getByText("Component testing is done with react-testing-library")
   expect(element).toBeDefined()
+
+})
+
+test("renders all content of a blog when viewed", () => {
+  const blog= {
+    title: "Component testing is done with react-testing-library",
+    url: "www.test.com",
+    author: "testuser",
+    likes: "999"
+  }
+
+  const mockHandler = jest.fn()
+
+  render(
+    <Blog blog={blog} visible={true} />
+  )
+
+  const url = screen.getByText("www.test.com")
+  expect(url).toBeDefined()
+
+  const author = screen.getByText("testuser")
+  expect(author).toBeDefined()
+
+  const likes = screen.getByText("likes: 999")
+  expect(likes).toBeDefined()
 
 })
