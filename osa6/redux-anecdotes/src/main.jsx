8973c537ca65d215c95e98c14bfee5806ee2e1f7
v3xj <vjbrah@gmail.com>
2023-11-16 23:23:26 +0200

    6.14 GET anecdotes from JSON server

--- osa6/redux-anecdotes/src/main.jsx
+++ osa6/redux-anecdotes/src/main.jsx
@@ -2,6 +2,12 @@ import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import App from './App'
 import store from './store'
+import anecdoteService from '/services/anecdotes'
+import { setAnecdotes } from './reducers/anecdoteReducer'
+
+anecdoteService.getAll().then(anecdotes =>
+  store.dispatch(setAnecdotes(anecdotes))
+)
 
 console.log('STATE BEFORE ANYTHING: ', store.getState())
 
